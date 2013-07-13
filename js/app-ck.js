9115e40ed725b0a8209ba2c4a8a178fa3a268067
Shajed Evan <evanshajed@gmail.com>
2013-07-13 21:31:04 +0600

    Fonts-Icon Pack, Nav components and more

--- js/app-ck.js
+++ js/app-ck.js
@@ -1,4 +1,4 @@
 /* **********************************************
      Custom App JavaScripts
 **********************************************///
-jQuery(function(e){e(".bxslider").bxSlider({adaptiveHeight:!0,mode:"fade",captions:!0})});
+jQuery(function(e){e(".bxslider").bxSlider({adaptiveHeight:!0,mode:"fade",captions:!0});e("body").addClass("js");var t=e("#side-menu"),n=e(".side-menu-link"),r=e("#wrap");n.click(function(){n.toggleClass("active");r.toggleClass("active");return!1})});
--- js/app.js
+++ js/app.js
@@ -8,4 +8,17 @@
         mode: 'fade',
         captions: true
     });
+
+    // Off Canvas
+    $('body').addClass('js');
+
+    var $menu = $('#side-menu'),
+      $menulink = $('.side-menu-link'),
+      $wrap = $('#wrap');
+
+    $menulink.click(function() {
+      $menulink.toggleClass('active');
+      $wrap.toggleClass('active');
+      return false;
+    });
  });
